78ba34e0eea35a5370980501702aebced9007d4a
solennement <77517965+solennement@users.noreply.github.com>
2021-01-20 11:39:09 +0100

    debut

---
+++ ProjetPOO2021/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProjetPOO2021
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
